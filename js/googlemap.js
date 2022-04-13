8d0b4f36d471c816094cf82521a3abed0a249700
Cngo2 <97567147+Cngo2@users.noreply.github.com>
2022-04-12 22:36:07 -0500

    changes

--- js/googlemap.js
+++ js/googlemap.js
@@ -1,16 +1,16 @@
+var canvas;
 function initMap() {
-  var map;
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 41.83699898857421, lng: -87.62827682574186},
-    zoom: 18
+  canvas = new google.maps.Map(document.getElementById('canvas'), {
+  center: {lat: 41.83699898857421, lng: -87.62827682574186},
+  zoom: 18
   });
 
   var marker = new google.maps.Marker({
-    position: new google.maps.LatLng(41.83698799755188, -87.62826743801091),
-    map, 
+    position: new google.maps.LatLng(41.83698799755188, -87.62826743801091), 
+    map: canvas,
     title: "Kaplan IIT",
     animation: google.maps.Animation.DROP,
-    icon: 'media/batman.png'
+    icon: './media/batman.png'
   });
 
   var infowindow = new google.maps.InfoWindow({
@@ -18,15 +18,15 @@ function initMap() {
   });
 
   google.maps.event.addListener(marker, 'mouseover', function() {
-    infowindow.open(map, marker);
+    infowindow.open(canvas, marker);
   });
 
   var marker2 = new google.maps.Marker({
     position: new google.maps.LatLng(41.835625166891816, -87.6286190237774),
-    map, 
+    map: canvas,
     title: "Hermann Hall IIT",
     animation: google.maps.Animation.DROP,
-    icon: 'media/spiderman.png'
+    icon: './media/spiderman.png'
   });
 
   var infowindow2 = new google.maps.InfoWindow({
@@ -34,7 +34,7 @@ function initMap() {
   });
 
   google.maps.event.addListener(marker2, 'mouseover', function() {
-    infowindow2.open(map, marker2);
+    infowindow2.open(canvas, marker2);
   });
 }
 google.maps.event.addDomListener(window, 'load', initMap);
